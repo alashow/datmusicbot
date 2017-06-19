962b8a71684d56c91aa6485570b6ff9d5788e6a1
Alashov Berkeli <yunus.alashow@gmail.com>
2017-06-19 17:29:20 +0500

    allow to override endpoint from env

--- datmusic/constants.py
+++ datmusic/constants.py
@@ -1,3 +1,5 @@
+import os
+
 __version__ = "1.2.0"
-DATMUSIC_API_ENDPOINT = "https://api.datmusic.xyz/search/"
+DATMUSIC_API_ENDPOINT = os.getenv("DATMUSIC_API_ENDPOINT", "https://datmusic-api.dev/search/")
 INLINE_QUERY_CACHE_TIME = 12 * 30 * 24 * 60 * 60; # 1 year
